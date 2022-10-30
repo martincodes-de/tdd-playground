689bfc131a6e900e48c4391a37ea0f66151d1d3d
martincodes <martin.schneider@techniknews.net>
2022-10-30 15:36:25 +0100

    add more testcases
    success

--- src/Codekatas/FizzBuzz/FizzBuzz.php
+++ src/Codekatas/FizzBuzz/FizzBuzz.php
@@ -7,8 +7,32 @@ namespace Codekatas\FizzBuzz;
 
 class FizzBuzz
 {
-    public function play(): array
+    public function play(int $start = 1, int $end = 2): array
     {
-        return [];
+        $numbers = range($start, $end);
+
+        foreach ($numbers as &$number) {
+            $replacedValue = $this->renameNumberIfNeeded($number);
+            $number = is_numeric($replacedValue) ? (int) $replacedValue : $replacedValue;
+        }
+
+        return $numbers;
+    }
+
+    private function renameNumberIfNeeded(int $number): string
+    {
+        if (($number % 3) === 0 && ($number % 5) === 0) {
+            return "FizzBuzz";
+        }
+
+        if (($number % 3) === 0) {
+            return "Fizz";
+        }
+
+        if (($number % 5) === 0) {
+            return "Buzz";
+        }
+
+        return (string) $number;
     }
 }
--- tests/Codekatas/FizzBuzz/FizzBuzzTest.php
+++ tests/Codekatas/FizzBuzz/FizzBuzzTest.php
@@ -12,8 +12,53 @@ class FizzBuzzTest extends TestCase
     /** @test */
     public function fizzbuzz_play_returnsArray()
     {
+        // Arrange
         $fizzBuzz = new FizzBuzz();
+
+        //Act
         $returnValue = $fizzBuzz->play();
+
+        //Assert
         $this->assertIsArray($returnValue);
+    }
+
+    /** @test */
+    public function fizzbuzz_play_returnsNumbersInArrayFromOneToZero()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $result = $fizzBuzz->play();
+
+        $this->assertEquals([1, 2], $result);
+    }
+
+    /** @test */
+    public function fizzbuzz_play_returnsNumbersInArrayWhereMultiplesOfThreeIsFizz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $numbers = $fizzBuzz->play(1, 3);
+
+        $this->assertEquals([1, 2, "Fizz"], $numbers);
+    }
+
+    /** @test */
+    public function fizzbuzz_play_returnsNumbersInArrayWhereMultiplesOfFiveIsBuzz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $numbers = $fizzBuzz->play(1, 5);
+
+        $this->assertEquals([1, 2, "Fizz", 4, "Buzz"], $numbers);
+    }
+
+    /** @test */
+    public function fizzbuzz_play_returnsNumbersInArrayWhereMultiplesOfThreeAndFiveIsFizzBuzz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $numbers = $fizzBuzz->play(1, 15);
+
+        $this->assertEquals([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"], $numbers);
     }
 }
